c7c113c68d16a39ec910cf032bde15f74cfa8a72
Mihir <mihirsuks@gmail.com>
2019-06-27 15:34:47 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Main Js App
+var http = require("http"); // Create HTTP object
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(80);
